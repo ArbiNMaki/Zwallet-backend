700ac6e0b944a7d50c50333b3ff007f92df63f0f
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2021-02-03 08:24:56 +0700

    Delete src directory

--- src/router/topup.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const topupController = require('../controllers/topup')
-
-router
-    .get('/', topupController.getAllTopUp)
-    .post('/', topupController.chargeTopUp)
-    
-module.exports = router
--- src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { loginUser, sendEmail, verification} = require('../controllers/authController')
-
-router
-.post('/login',loginUser)
-.post('/email',sendEmail)
-.get('/verify/:id',verification)
-module.exports = router
